b8e330b8c5c1b2550250b6e331587e68372be41c
rakibul875 <rakibulislamashik78@gmail.com>
2026-05-08 10:16:29 +0600

    instsll a pakeg

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const app = express()
-const port =process.env.PORT ||3000
+const port =process.env.PORT ||8000
+
+const user=[
+    {id: 1, name:'John',age:30},
+    {id: 2, name:'Jane',age:25},
+    {id: 3, name:'Bob',age:35},
+]
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -9,7 +15,11 @@ app.get('/about', (req, res) => {
   res.send('Hello from about section')
 })
 app.get('/user', (req, res) => {
-  res.send('Hello from user section')
+  res.send(user)
+})
+app.post('/user', (req, res) => {
+  console.log('Post method is working')
+  res.send('Post method is working')
 })
 
 app.listen(port, () => {
